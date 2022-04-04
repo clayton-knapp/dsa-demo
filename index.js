5a33fb9bfd3757a45f9ffe8b8c2321ebedf180c3
Clayton Knapp <csjknapp@gmail.com>
2022-04-04 10:51:09 -0700

    challenge 3: evenish or oddish

--- index.js
+++ index.js
@@ -11,9 +11,32 @@ function reverseLettersOfString(sentence) {
 }
 
 const reversedSentence = reverseLettersOfString(exampleSentence);
-console.log(reversedSentence);
+// console.log(reversedSentence);
 
 
 //note reverse only works on arrays not string
+
+
+function oddishOrEvenish(number) {
+  //turn number into string then split
+  const string = String(number);
+  // console.log('string', string);
+  const stringArr = string.split('');
+  let x = 0;
+  stringArr.map((string) => {
+    x += Number(string);
+    // console.log('x', x);
+    // return x;
+  });
+  return x % 2 === 0 ? 'Evenish' : 'Oddish';
+}
+
+const number1 = 121;
+const number2 = 41;
+const num1EvenOrOdd = oddishOrEvenish(number1);
+const num2EvenOrOdd = oddishOrEvenish(number2);
+
+console.log('num2EvenOrOdd', num2EvenOrOdd);
+console.log('num2EvenOrOdd', num2EvenOrOdd);
 
 
